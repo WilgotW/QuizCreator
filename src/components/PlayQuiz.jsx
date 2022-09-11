2da074c333de9cb4565475654d851b620662e170
Wilgot <wilgotwardell@hotmail.com>
2022-09-11 14:49:27 +0200

    progress

---
+++ src/components/PlayQuiz.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PlayQuiz = ({quizes, quizTitle, quizQuestions}) =>{
+  return (
+    <div>
+        <h1>{quizes.title}</h1>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default PlayQuiz
--- src/components/QuizCreator.jsx
+++ src/components/QuizCreator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PlayQuiz from './PlayQuiz';
 import QuizFormat from './QuizFormat';
 
 function QuizCreator() {
@@ -11,11 +12,23 @@ function QuizCreator() {
 
         btnDisable == true ? setBtnDisable(false) : setBtnDisable(true);
     }
+
+    const showQuiz = () => {
+        console.log(quizes);
+    }
   return (
     <div>
         <h1>Quiz Creator</h1>
         <button onClick={addQuiz} disabled={btnDisable}>Create Quiz</button>
         <QuizFormat quizes={quizes}></QuizFormat>
+        <button onClick={showQuiz}> Show Quizes</button>
+        
+        
+        <div>
+            <PlayQuiz
+                quizes={quizes}
+            ></PlayQuiz>
+        </div>
     </div>
   )
 }
