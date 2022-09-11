abca74e0c7f16ac80891847c8277c5d4b253bf05
Wilgot <wilgotwardell@hotmail.com>
2022-09-11 14:04:26 +0200

    disable button

--- src/components/QuizCreator.jsx
+++ src/components/QuizCreator.jsx
@@ -3,15 +3,18 @@ import QuizFormat from './QuizFormat';
 
 function QuizCreator() {
     const [quizes, setQuizes] = useState([]);
+    const [btnDisable, setBtnDisable] = useState(false);
 
-    const addQuiz = quiz => {
+    const addQuiz = quiz => { 
         const newQuizes = [quiz, ...quizes];
         setQuizes(newQuizes);
+
+        btnDisable == true ? setBtnDisable(false) : setBtnDisable(true);
     }
   return (
     <div>
         <h1>Quiz Creator</h1>
-        <button onClick={addQuiz}>Create Quiz</button>
+        <button onClick={addQuiz} disabled={btnDisable}>Create Quiz</button>
         <QuizFormat quizes={quizes}></QuizFormat>
     </div>
   )
